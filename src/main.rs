049b4ac3a77074134098dd22e00d8188f10a9b3e
Sundara Vishnu <svishnus@umich.edu>
2023-03-31 11:02:00 -0400

    Update gitignore and play around in main

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,9 @@
 fn main() {
     println!("Hello, world!");
+    let mut x = 25;
+    let y = &mut x;
+    *y = *y + 1;
+    println!("{}", *y + 1);
+    let z: String = String::from("Hello, World!");
+    println!("{}", z);
 }
