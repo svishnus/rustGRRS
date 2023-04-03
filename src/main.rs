e3db440534ac42dbf86e35c56f8b495ee2232ba4
Sundara Vishnu <svishnus@umich.edu>
2023-04-03 13:44:12 -0400

    Push partial implementation

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,28 @@
+#![allow(unused)]
+use std::fmt::format;
+
+use anyhow::{Context, Result};
+use clap::Parser;
+
+#[derive(Parser, Debug)]
+struct Cli {
+    pattern: String,
+    path: std::path::PathBuf,
+}
+
 fn main() {
-    println!("Hello, world!");
-    let mut x = 25;
-    let y = &mut x;
-    *y = *y + 1;
-    println!("{}", *y + 1);
-    let z: String = String::from("Hello, World!");
-    println!("{}", z);
+    let args = Cli::parse();
+    println!("{:?}", args);
+    let content = std::fs::read_to_string(&args.path).expect("Could not open file");
+    // println!("file content {}", content);
+
+    find_matches(&content, &args.pattern);
+}
+
+fn find_matches(content: &str, pattern: &str) {
+    for line in content.lines() {
+        if line.contains(pattern) {
+            println!("{}", line);
+        }
+    }
 }
